c61d989d20b80a18ecb418bd95762d339ec82ba8
Sebastian Geisler <sgeisler@wh2.tu-dresden.de>
2019-11-14 16:53:06 +0100

    impl decoding of where39 locations

---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+extern crate geo;
+
+mod wordlist;
+
+use geo::Point;
+use wordlist::WORDLIST;
+
+const TILES_SIZES: &[f64] = &[5.625, 0.125, 0.00277777777, 0.00006172839, 0.00000308642];
+const RASTER_SIZE: &[(usize, usize)] = &[(32, 64), (45, 45), (45, 45), (45, 45), (20, 20)];
+
+fn get_word_idx<'a>(word: &'a str) -> Result<usize, Error<'a>> {
+    WORDLIST.iter()
+        .enumerate()
+        .find(|(_, w)| **w == word)
+        .map(|(idx, _)| idx)
+        .ok_or(Error::InvalidWord(word))
+}
+
+pub fn from_words<'a, I, S>(words: I) -> Result<Point<f64>, Error<'a>>
+where I: Iterator<Item=&'a S> + ExactSizeIterator,
+      S: AsRef<str> + 'a + ?Sized
+{
+    if words.len() > 5 {
+        return Err(Error::TooManyWords(words.len()))
+    }
+
+    let (lat, long) = words.enumerate()
+        .map(|(idx, word)| {
+            let word_idx_limit = RASTER_SIZE[idx].0 * RASTER_SIZE[idx].1;
+            let word_idx = get_word_idx(word.as_ref())?;
+
+            if word_idx >= word_idx_limit {
+                return Err(Error::InvalidWord(word.as_ref()));
+            }
+
+            let lat_idx = word_idx / RASTER_SIZE[idx].1;
+            let long_idx = word_idx % RASTER_SIZE[idx].1;
+
+            Ok((
+                (lat_idx as f64) * TILES_SIZES[idx],
+                (long_idx as f64) * TILES_SIZES[idx],
+            ))
+        })
+        .try_fold((-90f64, -180f64), |acc, coords| {
+            match coords {
+                Ok((lat, long)) => Ok((acc.0 + lat, acc.1 + long)),
+                Err(e) => Err(e)
+            }
+        })?;
+
+    Ok(Point::new(lat, long))
+}
+
+#[derive(Debug)]
+pub enum Error<'a> {
+    InvalidWord(&'a str),
+    TooManyWords(usize),
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_words_to_coords() {
+        let words = vec!["slush", "battle", "damage", "dentist"];
+
+        let coords = super::from_words(words.iter()).unwrap();
+        assert_eq!(coords.y(), 13.72333333297f64);
+        assert_eq!(coords.x(), 51.02561728383f64);
+    }
+}
