2552eb821fe0db966fafd59af10b8ec4188aa291
Sebastian Geisler <sebastian@blockstream.io>
2019-11-14 23:37:43 +0100

    impl to_words

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 #![no_std]
 
+extern crate core;
+extern crate libm;
+
 mod wordlist;
 
+use core::convert::TryInto;
+use libm::F64Ext;
 use wordlist::WORDLIST;
 
 const TILES_SIZES: &[f64] = &[5.625, 0.125, 0.00277777777, 0.00006172839, 0.00000308642];
@@ -50,14 +55,41 @@ where I: Iterator<Item=&'a S> + ExactSizeIterator,
     Ok((lat, lng))
 }
 
+fn to_words(lat: f64, lng: f64) -> Result<[&'static str; 5], Error<'static>> {
+    if (-90f64..=90f64).contains(&lat) == false || (-180f64..=180f64).contains(&lng) == false {
+        return Err(Error::CoordinatesOutOfRange)
+    }
+
+    let mut lat = lat + 90f64;
+    let mut lng = ((lng - 180f64) % 360f64) + 360f64;
+
+    let mut code = [""; 5];
+
+    for (idx, code_word) in code.iter_mut().enumerate() {
+        let lat_idx_f64 = (lat / TILES_SIZES[idx]).floor();
+        let lng_idx_f64 = (lng / TILES_SIZES[idx]).floor();
+
+        lat -= lat_idx_f64 * TILES_SIZES[idx];
+        lng -= lng_idx_f64 * TILES_SIZES[idx];
+
+        let word_idx = (lat_idx_f64 as usize) * RASTER_SIZE[idx].1 + (lng_idx_f64 as usize);
+        *code_word = WORDLIST[word_idx];
+    }
+
+    Ok(code)
+}
+
 #[derive(Debug)]
 pub enum Error<'a> {
     InvalidWord(&'a str),
     TooManyWords(usize),
+    CoordinatesOutOfRange
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::to_words;
+
     #[test]
     fn test_words_to_coords() {
         let words = &["slush", "battle", "damage", "dentist"][..];
@@ -65,5 +97,13 @@ mod tests {
         let (lat, lng) = super::from_words(words.iter()).unwrap();
         assert_eq!(lat, 51.02561728383f64);
         assert_eq!(lng, 13.72333333297f64);
+    }
+
+    #[test]
+    fn test_coords_to_words() {
+        let code = to_words(51.02561728383f64, 13.72333333297f64).unwrap();
+        let expected = &["slush", "battle", "damage", "dentist"][..];
+
+        assert_eq!(expected, &code[..4]);
     }
 }
